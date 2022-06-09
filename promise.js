844b83d4e8dfec41a2ee73fe18f730cf8b0b50a3
fshdq <m.farras@rocketmail.com>
2022-06-09 23:03:55 +0700

    done

--- promise.js
+++ promise.js
@@ -1,7 +1,24 @@
 const { promiseTheaterIXX, promiseTheaterVGC } = require("./external.js");
 
 // TODO: Buat fungsi promiseOutput sesuai ketentuan readme
-const promiseOutput = null;
+async function promiseOutput(param) {
+  try {
+    const resIXX = await promiseTheaterIXX(param);
+    const resVGC = await promiseTheaterVGC(param);
+
+    const filterIXX = resIXX.filter(function (judul) {
+      return judul.hasil === param;
+    });
+
+    const filterVGC = resVGC.filter(function (judul) {
+      return judul.hasil === param;
+    });
+
+    return filterIXX.length += filterVGC.length;
+  } catch (error) {
+    console.log(error);
+  }
+}
 
 module.exports = {
   promiseOutput,
